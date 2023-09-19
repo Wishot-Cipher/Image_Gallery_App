b8ba9caf23d84c9b28a4bdddefa7ff7b8da8af1c
Alom Wisdom <ogwisdom27@gmail.com>
2023-09-19 17:45:51 +0100

    updated the drag and drop to be working on mobile screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-// App.jsx
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import PrivateRoute from './components/Authentication/PrivateRoute';
+// import PrivateRoute from './components/Authentication/PrivateRoute';
 import Gallery from './pages/Gallery';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import { DndProvider } from 'react-dnd';
+import { DndProvider, MultiBackend } from 'react-dnd-multi-backend';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { TouchTransition, MouseTransition } from 'react-dnd-multi-backend';
+
+const HTML5toTouch = {
+  backends: [
+    {
+      id: "html5",
+      backend: HTML5Backend,
+      transition: MouseTransition,
+    },
+    {
+      id: "touch",
+      backend: TouchBackend,
+      options: { enableMouseEvents: true },
+      transition: TouchTransition,
+      preview:true
+    },
+  ],
+};
 
 const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
